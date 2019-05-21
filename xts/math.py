86bdd398980af28812a37a7022b063fba09e3f44
Philipp Schmidt <p.schmidt@uni-kassel.de>
2019-05-21 14:47:56 +0200

    math: Add a number of generic function related to array operations, common distributions and peak finding

--- xts/math.py
+++ xts/math.py
@@ -10,6 +10,10 @@ def find_nearest_index(array, value):
     return (np.abs(array - value)).argmin()
 
 
+def find_nearest_value(array, value):
+    return array[find_nearest_index(array, value)]
+
+
 def identity(x):
     """Identity function.
 
@@ -24,8 +28,8 @@ def identity(x):
     return x
 
 
-def project_pimms_hits(xy, shape, dtype=None):
-    """Project an array of xy PImMS positions onto a matrix.
+def project_2d_hits(xy, shape, dtype=None):
+    """Project an array of xy positions onto a matrix, i.e. 2d binning.
 
     Args:
         xy (array_like): Input array of shape NxM, M ≥ 2.
@@ -33,7 +37,7 @@ def project_pimms_hits(xy, shape, dtype=None):
         dtype (data-type): Output matrix data type.
 
     Returns:
-        ndarray: The output matrix shaped like PIMMS_SHAPE.
+        ndarray: The output matrix
 
     """
 
@@ -117,6 +121,63 @@ def covariance(X, Y=None, r_corr=1.0, r_uncorr=1.0):
     res_uncorr =  r_uncorr   * np.dot(Xsum.T, Ysum)
 
     return res_corr - res_uncorr
+
+
+def radial_mask(shape, center=None, min_radius=0, max_radius=float('inf')):
+    """Generate a radial mask.
+
+    Args:
+        shape (tuple): Mask shape as (rows, columns).
+        center (tuple, optional): Center point, (rows//2, columns//2) is
+            used if omitted.
+        min_radius (float, optional): Lower radial bound, 0 if omitted.
+        max_radius (float, otpional): Upper radial bound, inf if omitted.
+
+    Returns:
+        (ndarray): Boolean mask array.
+
+    """
+
+    if center is None:
+        center = (shape[0] // 2, shape[1] // 2)
+
+    grid_y, grid_x = np.ogrid[:shape[0], :shape[1]]
+    radial_grid = (grid_x - center[1])**2 + (grid_y - center[0])**2
+    mask = (min_radius**2 <= radial_grid) * (radial_grid <= max_radius**2)
+
+    return mask
+
+
+def apply_radial_mask(data, value=None, **kwargs):
+    """Apply a radial mask.
+
+    A shorthand function for radial_mask(), a radial mask is generated and
+    immediately applied to either clear all points outside the mask or
+    set those inside the mask to a specific value.
+
+    Any additiona keyword arguments are passed to radial_mask().
+
+    Args:
+        data (ndarray): Data array to mask
+        value (float or None, optional): Set all points inside the mask to
+            this value or clear all remaining points if None, defaults to
+            None.
+
+    Returns:
+        (ndarray) Masked data array
+
+    """
+
+    mask = radial_mask(data.shape, **kwargs)
+
+    new_data = data.copy()
+
+    if value is None:
+        new_data[~mask] = 0
+    else:
+        new_data[mask] = value
+
+    return new_data
 
 
 def theta_mask(shape, center=None, min_angle=None, max_angle=None,
@@ -236,11 +297,67 @@ def apply_theta_mask(data, value=None, **kwargs):
     return new_data
 
 
+def gaussian(x, y0, A, μ, σ):
+    """Normalized gaussian distribution, i.e. normal distribution.
+
+    Args:
+        x (array_like, real): Function argument
+        y0 (real): Vertical offset
+        A (real): Amplitude
+        μ (real): Expected value
+        σ (real): Standard deviation
+
+    Returns:
+        Function value(s)
+
+    """
+
+    return y0 + (A / (σ * np.sqrt(2*np.pi))) \
+        * np.exp( -(x - μ)**2 / (2 * σ**2) )
+
+
+def gaussian2d(x, y, z0, A, μ_x, μ_y, σ_x, σ_y):
+    """Normalized 2d gaussian distribution.
+
+    Args:
+        x, y (array_like, real): Function arguments
+        z0 (real): Vertical offset
+        μ_x, μ_y (real): Expected values
+        σ_x, σ_y (real): Standard deviations
+
+    Returns:
+        Function value(s)
+
+    """
+
+    return z0 + (A/(σ_x * σ_y * 2*np.pi)) \
+        * np.exp( - (x[None, :] - μ_x)**2 / (2 * σ_x**2)
+                  - (y[:, None] - μ_y)**2 / (2 * σ_y**2) )
+
+
+def lorentzian(x, y0, A, x0, γ):
+    """Normalized lorentzian distribution, i.e. cauchy distribution.
+
+    Args:
+        x (array_like, real): Function argument
+        y0 (real): Vertical offset
+        A (real): Amplitude
+        x0 (real): Location parameter
+        γ (real): Scale parameter
+
+   Returns:
+       Function value
+
+    """
+
+    return y0 + (A/(np.pi*γ)) * (γ**2/((x - x0)**2 + γ**2))
+
+
 def angular_dist(ϑ, σ, β, P1, λ):
     """Electron angular distribution with Stokes parameter.
 
     Args:
-        ϑ (real): Emission angle
+        ϑ (array_like, real): Emission angle
         σ (real): Integrated cross section
         β (real): Beta parameter -1 < β < 2
         P1 (real): First Stoke's parameter
@@ -377,3 +494,195 @@ def curve_fit(f, x, y, p0=None, bounds=None, label=None, verbose=True, *args,
                                                     coeffs[i], errors[i]))
 
     return coeffs, errors, r_squared, cov
+
+
+def cfd(signal, threshold=150, fraction=1.0, delay=25, width=0, zero=0.0):
+    """Constant fraction discrimator.
+
+    Preliminary implementation in pure python for debugging purposes.
+
+    Args:
+        signal (array_like): Input signal.
+        threshold (float): Trigger threshold.
+        fraction (float, optional): Fraction value.
+        delay (int or float, optional): Delay value.
+        width (int, optional): Output pulse width, any further edges
+            within the width of an edge are suppressed.
+
+    Returns:
+        edge_indice (array_like): Peak position for all edges.
+        edge_heights (array_like): Peak height for all edges.
+        cfd_signal (array_like): superimposed CFD signal.
+    """
+
+    # Delay and invert
+    if isinstance(delay, int) or delay.is_integer():
+        # Optimization for integer, MUCH faster than the scipy call
+
+        cfd_signal = np.array(signal, dtype=np.float64)
+        cfd_signal[delay:] -= fraction * signal[:-delay]
+        cfd_signal[:delay] -= fraction * signal[1]
+    else:
+        cfd_signal = signal - shift(signal * fraction, delay, mode="nearest")
+
+    # Edge detection
+    try:
+        if threshold > 0:
+            edges_bool = cfd_signal < zero
+            above_threshold = signal[:-1] > threshold
+
+        else:
+            edges_bool = cfd_signal > zero
+            above_threshold = signal[:-1] < threshold
+
+        zero_crossings = edges_bool[1:] != edges_bool[:-1]
+
+        edge_indices = (zero_crossings & above_threshold).nonzero()[0]
+
+    except IndexError:
+        edge_indices = np.empty((0,), dtype=np.float32 if interpolate
+                                            else np.int32)
+
+    if len(edge_indices) > 0:
+        # Width suppression
+        if width > 0:
+            edge_diffs = edge_indices[1:] - edge_indices[:-1]
+            edge_mask = np.ones((len(edge_diffs) + 1,), dtype=bool)
+
+            try:
+                for edge_idx in (edge_diffs < width).nonzero()[0]:
+                    # Re-check the condition, as we modify the array
+                    if edge_diffs[edge_idx] < width:
+                        edge_mask[edge_idx+1] = False
+                        edge_diffs[edge_idx+1] += edge_diffs[edge_idx]
+
+            except IndexError:
+                # Might be thrown on the last found edge
+                pass
+
+            edge_indices = edge_indices[edge_mask]
+
+        new_indices = np.empty_like(edge_indices, dtype=np.float32)
+
+        # Interpolation
+        if threshold > 0:
+            for i in range(len(edge_indices)):
+                x = edge_indices[i]
+                new_indices[i] = x + cfd_signal[x]/(cfd_signal[x]
+                    - cfd_signal[x+1])
+        else:
+            for i in range(len(edge_indices)):
+                x = edge_indices[i]
+                new_indices[i] = x - cfd_signal[x]/(cfd_signal[x+1]
+                    - cfd_signal[x])
+
+        edge_indices = new_indices
+
+        # Peak heights
+        edge_heights = np.empty_like(edge_indices, dtype=signal.dtype)
+
+        if threshold > 0:
+            for i in range(len(edge_indices)):
+                edge_idx = int(edge_indices[i])
+                try:
+                    edge_heights[i] = \
+                        signal[edge_idx-delay:edge_idx+delay].max()
+                except ValueError:
+                    edge_heights[i] = 0
+        else:
+            for i in range(len(edge_indices)):
+                edge_idx = int(edge_indices[i])
+                try:
+                    edge_heights[i] = \
+                        signal[edge_idx-delay:edge_idx+delay].min()
+                except ValueError:
+                    edge_heights[i] = 0
+
+    else:
+        edge_heights = np.empty((0,), dtype=signal.dtype)
+
+    return edge_indices, edge_heights, cfd_signal
+
+
+def _is_unique_peak(peak_indices, peak_xs, peak_ys, n_peaks, cur_x, cur_y, search_size):
+    for i, peak_idx in zip(range(n_peaks), peak_indices):
+        if abs(cur_x - peak_xs[peak_idx]) <= search_size \
+                and abs(cur_y - peak_ys[peak_idx]) <= search_size:
+            return False
+
+    return True
+
+def _sort_peaks(data, peak_xs, peak_ys, all_indices, search_size, peak_indices, peak_coms_x, peak_coms_y):
+    n_peaks = 0
+
+    xy_shape = (6, 6)
+    xy_vals = np.arange(2*search_size)
+
+    for cur_idx in all_indices:
+        cur_x = peak_xs[cur_idx]
+        cur_y = peak_ys[cur_idx]
+
+        if is_unique_peak_py(peak_indices, peak_xs, peak_ys, n_peaks, cur_x, cur_y, search_size):
+            peak_indices[n_peaks] = cur_idx
+
+            peak_region = data[cur_y-search_size:cur_y+search_size,
+                               cur_x-search_size:cur_x+search_size]
+
+            if peak_region.shape != xy_shape:
+                x_vals = np.arange(peak_region.shape[1])
+                y_vals = np.arange(peak_region.shape[0])
+            else:
+                x_vals, y_vals = xy_vals, xy_vals
+
+            area = peak_region.sum()
+
+            if area == 0:
+                peak_coms_x[n_peaks] = cur_x
+                peak_coms_y[n_peaks] = cur_y
+            else:
+                peak_coms_x[n_peaks] = (cur_x - search_size + (peak_region.sum(0) * x_vals).sum() / area)
+                peak_coms_y[n_peaks] = (cur_y - search_size + (peak_region.sum(1) * y_vals).sum() / area)
+
+            n_peaks += 1
+
+    return n_peaks
+
+
+def find_2d_hits(data, threshold, search_size, sort_peaks=None):
+    # Get all points above the threshold
+    peak_ys, peak_xs = np.where(data > threshold)
+
+    # Peak indices sorted from the largest number down
+    peak_values = data[peak_ys, peak_xs]
+    all_indices = sorted(range(len(peak_xs)), key=lambda x: peak_values[x], reverse=True)
+
+    if sort_peaks is None:
+        try:
+            sort_peaks = sort_peaks_ntv
+        except NameError:
+            sort_peaks = _sort_peaks
+
+    if sort_peaks is sort_peaks_ntv:
+        # Ensure they are contiguous for C
+        data = np.ascontiguousarray(data)
+        all_indices = np.ascontiguousarray(all_indices)
+        peak_xs = np.ascontiguousarray(peak_xs)
+        peak_ys = np.ascontiguousarray(peak_ys)
+
+    # Holds the indices of all unique peaks
+    peak_indices = np.empty_like(all_indices)
+
+    # Holds the proper center of mass of all unique peaks
+    peak_coms_x = np.empty((peak_xs.shape[0],), dtype=np.float64)
+    peak_coms_y = np.empty((peak_ys.shape[0],), dtype=np.float64)
+
+    n_peaks = sort_peaks(data, peak_xs, peak_ys, all_indices, search_size, peak_indices, peak_coms_x, peak_coms_y)
+
+    if n_peaks == 0:
+        return None
+
+    peak_indices = peak_indices[:n_peaks]
+    peak_coms_x = peak_coms_x[:n_peaks]
+    peak_coms_y = peak_coms_y[:n_peaks]
+
+    return peak_xs[peak_indices], peak_ys[peak_indices], peak_coms_x, peak_coms_y
