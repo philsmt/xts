1fa7e334b3276eb1e28056687b1812c8ee4022c3
Philipp Schmidt <p.schmidt@uni-kassel.de>
2019-12-05 12:01:04 +0100

    Update setup.py with git version tag and additional metadata

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+
 from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.build_ext import build_ext
@@ -25,17 +27,31 @@ else:
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+version_str = 'dev'
+
+try:
+    short_hash = subprocess.check_output(
+        ['git', 'rev-parse', '--short', 'HEAD'],
+        stderr=subprocess.STDOUT
+    )
+except Exception:
+    pass
+else:
+    if not short_hash.startswith(b'fatal'):
+        version_str = short_hash.decode('ascii').strip()
+
 setup(
     name = 'XTS',
-    version = '0.0.1',
+    version = version_str,
     author = 'Philipp Schmidt',
-    author_email = 'phil.smt@gmail.com',
+    author_email = 'philipp.schmidt@xfel.eu',
     description = ('An extendable toolkit for data analysis of single-shot data, e.g. at FEL experiments'),
     license = 'GPL',
-    packages = ['xts',],
+    packages = ['xts', 'xts.math'],
     long_description = read('README.md'),
     long_description_content_type='text/markdown',
     python_requires = '>=3.6',
+    install_requires = ['numpy', 'scipy'],
     classifiers = [
         'Topic :: Scientific/Engineering :: Physics',
         'Intended Audience :: Science/Research',
